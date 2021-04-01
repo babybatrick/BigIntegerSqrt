9fc46abf13d896335d8c84e8298f038bcc078dbc
Amin <74154304+Babybatrick@users.noreply.github.com>
2021-04-01 19:39:27 +0300

    1.0

--- BigIntegerSqrt/BigIntegerSqrt.cs
+++ BigIntegerSqrt/BigIntegerSqrt.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Numerics;
+
 namespace BigIntegerSqrt
 {
-    public class BigIntegerSqrt
+    public static class BigIntegerSqrt
     {
-        
+        public static BigInteger Sqrt(BigInteger number)
+        {
+            BigInteger _x = RandomBigIntegerOfLength((number.ToString().ToCharArray().Length / 2) - 1);
+			
+            try
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    _x = (_x + number / _x) / 2;
+                }
+                return _x;
+            }
+            catch (DivideByZeroException)
+            {
+                return 0;
+            }
+        }
+
+        private static BigInteger RandomBigIntegerOfLength(int length)
+        {
+            Random rand = new Random();
+
+            string _randomNumber = "";
+			
+            _randomNumber = String.Concat(_randomNumber, rand.Next(1, 10));
+		
+            for (int i = 0; i < length; i++)
+            {
+                _randomNumber = String.Concat(_randomNumber,rand.Next(10).ToString());
+            }
+            if (String.IsNullOrEmpty(_randomNumber) == false) return BigInteger.Parse(_randomNumber);
+            else return 0;
+        }
     }
 }
